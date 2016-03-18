c230a2f054dc6bc5a95a89225ec33521c83aac47
celenastefanie <celenastefanie@gmail.com>
2016-03-18 16:22:06 -0400

    i want to die

--- js/main.js
+++ js/main.js
@@ -4,12 +4,8 @@ $('html').on('keydown', function (e) {
   var randLeft = Math.random() * 1000;
   var $body = $('body');
 
-  $newDiv.addClass('ball');
-
   $body.append($newDiv);
-
-  $newDiv.css({
-    top: (randTop),
-    left: (randLeft)
-  });
+  $newDiv.addClass('ball');
+  $newDiv.css('top', randTop);
+  $newDiv.css('left', randLeft);
 });
